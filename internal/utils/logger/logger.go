3105b43904e183d00b841ff320bf957cb4791906
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-08-09 16:57:43 -0300

    read log level from env variable and set it

--- internal/utils/logger/logger.go
+++ internal/utils/logger/logger.go
@@ -3,12 +3,31 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"strings"
 )
 
 var baseLogger *slog.Logger
 
 func init() {
-	baseLogger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{}))
+
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "DEBUG"
+	}
+
+	var level slog.Level
+	switch strings.ToUpper(logLevel) {
+	case "DEBUG":
+		level = slog.LevelDebug
+	case "INFO":
+		level = slog.LevelInfo
+	case "WARN":
+		level = slog.LevelWarn
+	case "ERROR":
+		level = slog.LevelError
+	}
+
+	baseLogger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level}))
 }
 
 func GetLogger(prefix string) *slog.Logger {
