3b436b6c4ae77a7e4ecce442ae8bbd561536e8b8
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2024-08-12 18:03:21 -0300

    create migrations for the accounts and transactions

---
+++ db/migrations/000001_accounts_table.up.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE SCHEMA IF NOT EXISTS "dev";
+
+CREATE TABLE IF NOT EXISTS "dev".accounts (
+    id     UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    address VARCHAR(255) NOT NULL
+);
+
+INSERT INTO "dev".accounts (address) VALUES ('0110590920000001234567');
+
+
+COMMIT;
